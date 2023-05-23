46a3f22e5fe43146547165f5347391b878b9ec2a
Federico Reghenzani <federico.reghenzani@polimi.it>
2023-05-23 03:14:50 +0200

    Some progress on image loading

--- src/scenes/test1.js
+++ src/scenes/test1.js
@@ -1,20 +1,26 @@
 let stepLength = 3;
 let floorHeight = 600;
 
-function preload() {
+let img_background;
+
+function preload(s) {
     console.log("Executing preload()");
+    img_background = PP.assets.load_image(s, "assets/images/background.png");
 }
 
 
-function create() {
+function create(s) {
     console.log("Executing create()");
+    s.background0 = s.add.image(0, 0, img_background.id);
+    s.background0.setOrigin(0,0);
+
 }
 
-function update() {
+function update(s) {
     console.log("Executing update()");
 }
 
-function destroy() {
+function destroy(s) {
     console.log("Executing destroy()");
 }
 
