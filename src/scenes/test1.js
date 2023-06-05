f09b81eef2d4f80c215158c462eec5a44d44499d
Federico Reghenzani <federico@reghe.net>
2023-06-05 23:39:02 +0200

    [Dev] Spritesheets and other improvements

--- src/scenes/test1.js
+++ src/scenes/test1.js
@@ -2,18 +2,22 @@ let stepLength = 3;
 let floorHeight = 600;
 
 let img_background;
+let img_background_instance;
+
+let ss_player;
+
 
 function preload(s) {
     console.log("Executing preload()");
     img_background = PP.assets.load_image(s, "assets/images/background.png");
+    ss_player = PP.assets.load_spritesheet(s, "assets/images/spritesheet_scaled.png", 223, 190, 0, 4);
+
 }
 
 
 function create(s) {
     console.log("Executing create()");
-    s.background0 = s.add.image(0, 0, img_background.id);
-    s.background0.setOrigin(0,0);
-
+    img_background_instance = PP.assets.add_image(s, img_background, 0, 0, 0, 0);
 }
 
 function update(s) {
