bedac45bb436beecea78319fca0973d9acb757b2
Federico Reghenzani <federico@reghe.net>
2023-07-04 13:16:50 +0200

    Implementation of sprite animation start/stop

--- src/scenes/test1.js
+++ src/scenes/test1.js
@@ -5,7 +5,8 @@ let img_background;
 let img_background_instance;
 
 let ss_player;
-
+let player_instance;
+let frame_count = 0;
 
 function preload(s) {
     console.log("Executing preload()");
@@ -18,10 +19,19 @@ function preload(s) {
 function create(s) {
     console.log("Executing create()");
     img_background_instance = PP.assets.image.add(s, img_background, 0, 0, 0, 0);
+
+    player_instance = PP.assets.sprite.add(s, ss_player, 0, 0, 0, 0);
+    PP.assets.sprite.animation_add(player_instance, "walk", 0, 10, 10, -1);
+    PP.assets.sprite.animation_play(player_instance, "walk");
 }
 
 function update(s) {
     console.log("Executing update()");
+
+    if (frame_count++ == 100) {
+        PP.assets.sprite.animation_stop(player_instance);
+    }
+
 }
 
 function destroy(s) {
