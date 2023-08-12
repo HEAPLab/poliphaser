ce8ff1b0503df121f188dd121abe0ef482aa8652
Federico Reghenzani <federico@reghe.net>
2023-08-12 08:13:29 +0200

    [Interactive] Fix mouse.add and create mouse.remove_all

--- src/scenes/test1.js
+++ src/scenes/test1.js
@@ -8,13 +8,16 @@ let ss_player;
 let player_instance;
 let frame_count = 0;
 
+function on_sprite_click() {
+    alert("Clicked!");
+}
+
 function preload(s) {
     console.log("Executing preload()");
     img_background = PP.assets.image.load(s, "assets/images/background.png");
     ss_player = PP.assets.sprite.load_spritesheet(s, "assets/images/spritesheet_scaled.png", 223, 190, 0, 4);
 
 }
-
 
 function create(s) {
     console.log("Executing create()");
@@ -23,6 +26,10 @@ function create(s) {
     player_instance = PP.assets.sprite.add(s, ss_player, 0, 0, 0, 0);
     PP.assets.sprite.animation_add(player_instance, "walk", 0, 10, 10, -1);
     PP.assets.sprite.animation_play(player_instance, "walk");
+
+    PP.interactive.mouse.add(player_instance, "pointerdown", on_sprite_click);
+    PP.interactive.mouse.remove_all(player_instance, "pointerdown");
+
 }
 
 function update(s) {
