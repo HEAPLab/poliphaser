77ee281ba709d97ae52c0e14095ab63b20b5f19e
Federico Reghenzani <federico.reghenzani@polimi.it>
2023-10-01 12:23:29 +0200

    [Examples] Scena 8

--- examples/set1/src/scenes/scene8.js
+++ examples/set1/src/scenes/scene8.js
@@ -8,9 +8,11 @@ let img_background_inst;
 let player;
 
 let player_is_moving = false;
+let player_is_jumping = false;
+let player_is_falling = false;
 
 function preload(s) {
-    console.log("Executing preload() - SCENE 7");
+    console.log("Executing preload() - SCENE 8");
 
     // Carichiamo gli asset grafici
     img_background = PP.assets.image.load(s, "assets/images/background.png");
@@ -22,23 +24,58 @@ function preload(s) {
 
 function create(s) {
     // Qui le istruzioni su cosa creare e dove nel mondo di gioco
-    console.log("Executing create() - SCENE 7");
+    console.log("Executing create() - SCENE 8");
 
     // Posizioniamo gli elementi nella scena
     img_background_inst = PP.assets.image.add(s, img_background, 0, 0, 0, 0);
     player     = PP.assets.sprite.add(s, ss_player, 100, floor_height, 0.5, 1);
     
     PP.assets.sprite.animation_add_list(player, "run", [6, 13, 20, 27, 34], 10, -1);    // Lista di frame, a 10 fps, inifito
+    PP.assets.sprite.animation_add(player, "jump_up", 36, 36, 10, 0);
+    PP.assets.sprite.animation_add(player, "jump_down", 42, 45, 10, 0);
+    PP.assets.sprite.animation_add(player, "stop", 21, 21, 10, 0);
+    PP.assets.sprite.animation_play(player, "stop");
+
+
 }
 
 function update(s) {
+
+    let should_stop = true;
+
     // Azioni che vengono eseguite a ogni frame del gioco
+
+    if(PP.interactive.kb.is_key_down(s, PP.KeyCodes.SPACE)  && player.geometry.y >= 0){
+        should_stop = false;
+        if(!player_is_jumping) {
+            // Avviamo l'animazione solo se essa non è già attiva
+            player_is_jumping = true;
+            player_is_moving  = true;
+            PP.assets.sprite.animation_play(player, "jump_up");
+        }
+        player.geometry.y -= 10; // Spostamento verso l'alto (salto)
+    } else {
+        // Se non sto premendo il tasto e "sto ancora volando"
+        // allora applico una "gravita' approssimativa"
+        if(player.geometry.y <= floor_height) {
+            player.geometry.y += 5; // Il player si sposta verso il basso
+            should_stop = false;
+
+            if(!player_is_falling) {
+                // Avviamo l'animazione solo se essa non è già attiva
+                player_is_falling  = true;
+                player_is_moving  = true;
+                PP.assets.sprite.animation_play(player, "jump_down");
+            }    
+        }
+    }
 
 
     // E' stato premuto il tasto freccia sinistra e il giocatore è a destra del limite sinistro del quadro?
     if(PP.interactive.kb.is_key_down(s, PP.KeyCodes.LEFT) && player.geometry.x >= 0) {
         player.geometry.flip_x = true;        // Volta il giocatore verso sinistra
         player.geometry.x     -= step_length; // Sposta il giocatore verso a sinistra
+        should_stop = false;
 
         if(!player_is_moving) {
             // Avviamo l'animazione solo se essa non è già attiva
@@ -49,6 +86,7 @@ function update(s) {
     else if(PP.interactive.kb.is_key_down(s, PP.KeyCodes.RIGHT) && player.geometry.x <= PP.game.config.canvas_width - player.geometry.display_width) {
         player.geometry.flip_x = false;       // Volta il giocatore verso destra
         player.geometry.x     += step_length; // Sposta il giocatore verso a destra
+        should_stop = false;
 
         if(!player_is_moving) {
             // Avviamo l'animazione solo se essa non è già attiva
@@ -56,18 +94,22 @@ function update(s) {
             PP.assets.sprite.animation_play(player, "run");
         }
     }
-    else {
-
+    
+    if(should_stop) {
         // Se non è premuto alcun tasto, allora disattiviamo l'animazione
         player_is_moving  = false;
-        PP.assets.sprite.animation_stop(player); // Ferma al frame corrente
+        player_is_jumping = false;
+        player_is_falling = false;
+        PP.assets.sprite.animation_play(player, "stop");
     }
+
+
 
 }
 
 function destroy(s) {
-    console.log("Executing destroy() - SCENE 7");
+    console.log("Executing destroy() - SCENE 8");
 
 }
 
-PP.scenes.add("scene7", preload, create, update, destroy);
+PP.scenes.add("scene8", preload, create, update, destroy);
