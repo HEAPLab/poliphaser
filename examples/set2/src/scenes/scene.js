53b422278f0e228b4f065f8a6637ddc5f7d4e4c9
Federico Reghenzani <federico.reghenzani@polimi.it>
2023-10-27 18:13:24 +0200

    Add collider

--- examples/set2/src/scenes/scene.js
+++ examples/set2/src/scenes/scene.js
@@ -3,6 +3,7 @@ let img_player;
 
 let player;
 let floor;
+let box;
 
 function preload(s) {
     console.log("Executing preload() - SCENE 1");
@@ -10,6 +11,10 @@ function preload(s) {
     // Carichiamo gli asset grafici
     img_background = PP.assets.image.load(s, "assets/images/background.png");
     img_player     = PP.assets.image.load(s, "assets/images/player.png");
+}
+
+function collider_test(s,a,b) {
+    console.log("Player colliding with the box!");
 }
 
 function create(s) {
@@ -20,6 +25,24 @@ function create(s) {
     player = PP.assets.image.add(s, img_player, 150, 630, 0.5, 1);
     
     PP.physics.add(s, player, PP.physics.type.DYNAMIC); 
+
+    PP.physics.set_velocity_x(player, 100);
+    PP.physics.set_velocity_y(player, -300);
+
+    //PP.physics.set_collide_world_bounds(player, true);
+
+    floor = PP.shapes.rectangle_add(s, 640, 650, 1280, 20, "0x000000", 1);
+    PP.physics.add(s, floor, PP.physics.type.STATIC); 
+
+    box = PP.shapes.rectangle_add(s, 800, 500, 100, 100, "0x0000FF", 1);
+    PP.physics.add(s, box, PP.physics.type.DYNAMIC); 
+
+
+    PP.physics.add_collider(s, player, floor); 
+    PP.physics.add_collider(s, box, floor); 
+
+
+    PP.physics.add_collider_f(s, player, box, collider_test); 
 
 }
 
